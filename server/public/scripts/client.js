4b427bdb4d08622bfff3b56b77daff9523bfe75c
Selena Carlson <selena.h.carlson@gmail.com>
2018-03-05 15:13:17 -0600

    everything is working, now to add a service

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,33 @@
 const app = angular.module('myApp', []);
 
 const messageController = app.controller('MessageController', ['$http', function($http){
-    console.log('mc')
+    let self = this;
+    self.newMessage = {};
+    self.messages = { list: [] }
+
+    self.getMessages = function(){
+        $http({
+            method: 'GET',
+            url: '/messages'
+        }).then(function(res){
+            self.messages.list = res.data;
+        }).catch(function(error){
+            console.log('error on get', error);
+        })
+    }
+
+    self.sendMessage = function(newMessage){
+        $http({
+            method: 'POST',
+            url: '/messages',
+            data: newMessage
+        }).then(function(res){
+            self.getMessages();
+        }).catch(function(error){
+            console.log('error on post');
+        })
+    }
+
+    self.getMessages();
 
 }])
--- server/routers/message-router.js
+++ server/routers/message-router.js
@@ -1,5 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema(
+    {
+        name: String,
+        text: String
+    }
+);
+
+const Message = mongoose.model('Message', MessageSchema, 'messages');
+
+router.get('/', function(req, res) {
+    Message.find({}, function(error, foundMessages){
+        if (error){
+            console.log('error on find messages', error);
+            res.sendStatus(500);
+        } else {
+            res.send(foundMessages);
+        }
+    })
+});
+
+router.post('/', function(req, res) {
+    let newMessage = new Message(req.body);
+    console.log('saving message', newMessage);
+    newMessage.save( function(error, savedMessage){
+        if (error){
+            console.log('error on add message', error)
+        } else {
+            res.sendStatus(200);
+        }
+    })
+});
+
 
 module.exports = router;
